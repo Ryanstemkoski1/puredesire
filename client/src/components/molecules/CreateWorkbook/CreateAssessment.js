9e5b7140c482b50ed843cd8be0df5b91fa313ae8
Ryanstemkoski1 <ryan.stemkoski@abectech.com>
2022-10-19 23:50:50 -0700

    assessment popup

--- client/src/components/molecules/CreateWorkbook/CreateAssessment.js
+++ client/src/components/molecules/CreateWorkbook/CreateAssessment.js
@@ -1,5 +1,6 @@
-import { Box, Modal } from '@mui/material'
+import { Box, Button, Divider, Grid, Modal, Typography } from '@mui/material'
 import React from 'react'
+import AddIcon from '@mui/icons-material/Add';
 
 export default function CreateAssessment({ workBookId, sectionId, open }) {
 
@@ -10,6 +11,77 @@ export default function CreateAssessment({ workBookId, sectionId, open }) {
     >
       <Box className="modal-wrap">
         <label className='label'>Assessment</label>
+        <Divider style={{ margin: "20px 0" }} />
+        <Box
+          className="dashed-border"
+          sx={{
+            minHeight: '130px',
+            borderRadius: '4px',
+            margin: '20px 0',
+            display: "flex",
+            alignItems: "center"
+          }}
+        >
+          <Grid container
+            spacing={10}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <div className='o_icon_wrapper'>
+                <AddIcon style={{ fill: "#FFA659" }} />
+              </div>
+              <Typography style={{ color: "#00000099", fontSize: "15px", paddingTop: "5px" }}>Add Text</Typography>
+            </Grid>
+            <Grid item>
+              <div className='o_icon_wrapper'>
+                <AddIcon style={{ fill: "#FFA659" }} />
+              </div>
+              <Typography style={{ color: "#00000099", fontSize: "15px", paddingTop: "5px" }}>Add Text Area</Typography>
+            </Grid>
+            <Grid item>
+              <div className='o_icon_wrapper'>
+                <AddIcon style={{ fill: "#FFA659" }} />
+              </div>
+              <Typography style={{ color: "#00000099", fontSize: "15px", paddingTop: "5px" }}>Rating(1-10)</Typography>
+            </Grid>
+            <Grid item>
+              <div className='o_icon_wrapper'>
+                <AddIcon style={{ fill: "#FFA659" }} />
+              </div>
+              <Typography style={{ color: "#00000099", fontSize: "15px", paddingTop: "5px" }}>Checkboxes</Typography>
+            </Grid>
+            <Grid item>
+              <div className='o_icon_wrapper'>
+                <AddIcon style={{ fill: "#FFA659" }} />
+              </div>
+              <Typography style={{ color: "#00000099", fontSize: "15px", paddingTop: "5px" }}>Radio Buttons</Typography>
+            </Grid>
+          </Grid>
+        </Box>
+        <Grid container
+          spacing={3}
+          direction="row"
+          justifyContent="flex-end"
+          style={{ marginTop: "0" }}
+          alignItems="center">
+          <Grid item align="right">
+            <Button
+              className="btn secondary"
+              onClick={open}
+            >
+              Cancel
+            </Button>
+          </Grid>
+          <Grid item align="right">
+            <Button
+              className="btn primary"
+              type="submit">
+              Save and Exit
+            </Button>
+          </Grid>
+        </Grid>
       </Box>
     </Modal>
   )
